e77c38ec3d6c319947993f494181bac652e3cbeb
Aleksandre Vardanidze <avardanidze@gmail.com>
2016-03-18 17:29:18 +0400

    Update MainTask.php

--- app/tasks/MainTask.php
+++ app/tasks/MainTask.php
@@ -81,7 +81,7 @@ class MainTask extends \Phalcon\CLI\Task
 
 			    $context = new ZMQContext();
 			    $socket = $context->getSocket(ZMQ::SOCKET_PUSH, 'my pusher');
-			    $socket->connect("tcp://192.168.0.109:5555");
+			    $socket->connect("tcp://localhost:5555");
 			    $socket->send(json_encode($entryData));
 
 		   } 
